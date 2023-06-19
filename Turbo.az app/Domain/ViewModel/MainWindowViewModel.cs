77fec6c8fc5ed5a881772725dcd32a17b2bd9fc7
Baxsh_cw07 <Baxsh_cw07@STHQ0128-13>
2023-06-19 20:35:34 +0400

    Added views element

--- Turbo.az app/Domain/ViewModel/MainWindowViewModel.cs
+++ Turbo.az app/Domain/ViewModel/MainWindowViewModel.cs
@@ -12,8 +12,10 @@ namespace Turbo.az_app.Domain.ViewModel
     public class MainWindowViewModel:BaseViewModel
     {
 
-        public RelayCommand VendorSelectionChanged { get; set; }
-
+        public RelayCommand VendorSelectionChangedCommand { get; set; }
+        public RelayCommand ModelSelectionChangedCommand { get; set; } 
+        public RelayCommand CitySelectionChangedCommand { get; set; }   
+        public RelayCommand BanTypeSelectionChangedCommand { get; set; }    
 
         private Brand selectedBrand;
 
@@ -31,6 +33,23 @@ namespace Turbo.az_app.Domain.ViewModel
             set { selectedModel = value; OnPropertyChanged(); }
         }
 
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set { selectedCity = value; OnPropertyChanged(); }
+        }
+
+        private BodyType selectedBanType;
+
+        public BodyType SelectedBanType
+        {
+            get { return selectedBanType; }
+            set { selectedBanType = value; OnPropertyChanged(); }
+        }
+
+
         private ObservableCollection<Brand> brands;
 
         public ObservableCollection<Brand> Brands
@@ -38,6 +57,40 @@ namespace Turbo.az_app.Domain.ViewModel
             get { return brands; }
             set { brands = value; OnPropertyChanged(); }
         }
+
+        private ObservableCollection<Model> models;
+
+        public ObservableCollection<Model> Models
+        {
+            get { return models; }
+            set { models = value; }
+        }
+
+        private ObservableCollection<City> cities;
+
+        public ObservableCollection<City> Cities
+        {
+            get { return cities; }
+            set { cities = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<BodyType> banTypes;
+
+        public ObservableCollection<BodyType> BanTypes
+        {
+            get { return banTypes; }
+            set { banTypes = value; OnPropertyChanged(); }
+        }
+
+
+        private bool brandSelected;
+
+        public bool BrandSelected
+        {
+            get { return brandSelected; }
+            set { brandSelected = value; OnPropertyChanged(); }
+        }
+
 
 
         public MainWindowViewModel()
