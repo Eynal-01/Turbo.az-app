52af73262b238a01bc9c26a4fe3398718729a527
Baxsh_cw07 <Baxsh_cw07@STHQ0127-08>
2023-06-19 19:00:23 +0400

    .

--- Turbo.az app/Domain/ViewModel/MainWindowViewModel.cs
+++ Turbo.az app/Domain/ViewModel/MainWindowViewModel.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Turbo.az_app.Command;
+using Turbo.az_app.Entities.Mapping;
 
 namespace Turbo.az_app.Domain.ViewModel
 {
     public class MainWindowViewModel:BaseViewModel
     {
+
+        public RelayCommand VendorSelectionChanged { get; set; }
+
+
+        private Brand selectedBrand;
+
+        public Brand SelectedBrand
+        {
+            get { return selectedBrand; }
+            set { selectedBrand = value; OnPropertyChanged(); }
+        }
+
+        private Model selectedModel;
+
+        public Model SelectedModel
+        {
+            get { return selectedModel; }
+            set { selectedModel = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<Brand> brands;
+
+        public ObservableCollection<Brand> Brands
+        {
+            get { return brands; }
+            set { brands = value; OnPropertyChanged(); }
+        }
+
+
         public MainWindowViewModel()
         {
-
+            
         }
     }
 }
